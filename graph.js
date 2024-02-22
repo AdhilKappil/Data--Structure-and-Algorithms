01b2a8dcd9c9d1fa1a4f2b8d81acba9008348426
AdhilKappil <adhilalikappil@gmail.com>
2024-02-22 09:43:28 +0530

    trie auto searching, balanced tree cheking

--- graph.js
+++ graph.js
@@ -2,11 +2,15 @@ class Graph{
     constructor(){
         this.list = {}
     }   
+
+    
     addVertex(vetex){
         if(!this.list[vetex]){
             this.list[vetex] = new Set();
         }
     }// o(1) T & S
+
+
     addEdge(vetex1, vetex2){
         if(!this.list[vetex1]){
             this.addVertex(vetex1)
@@ -17,19 +21,27 @@ class Graph{
         this.list[vetex1].add(vetex2);
         this.list[vetex2].add(vetex1)
     }// O(1) T S
+
+
     display(){
         for(let vetex in this.list){
             console.log(vetex+'->'+[...this.list[vetex]]);
         }
+
+
     }// O(V+E) T  O(1)S
     // searching
     hasEdge(vetex1, vetex2){
         return this.list[vetex1].has(vetex2) && this.list[vetex2].has(vetex1)
     }// O(1) T S
+
+
     deleteEdge(vetex1, vetex2){
         this.list[vetex1].delete(vetex2);
         this.list[vetex2].delete(vetex1)
     }// O(1) T S
+
+
     removeVertex(vetex){
         if(!this.list[vetex]){
             return 
@@ -39,6 +51,8 @@ class Graph{
         }
         delete this.list[vetex]
     }// time complexity depend on adjecent verticies O(V+E)   O(1)S
+
+
     BSF(start){
         const queue = [start];
         const visited = new Set();
@@ -55,6 +69,8 @@ class Graph{
         }
         return visited;  // O(v+e) T   O(v) S
     }
+
+
     DFS(start){
         const stack = [start];
         const visited = new Set();
--- trie.js
+++ trie.js
@@ -13,7 +13,7 @@ class Trie {
 
     insert(word) {
         let node = this.root;
-        for (let char of word) {
+        for (let char of word) {        
             if (!node.children[char]) {
                 node.children[char] = new TrieNode();
             }
@@ -36,18 +36,72 @@ class Trie {
         return node.isEnd;
     }
 
+    // === prefix chekking ====
+    startWith(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+          if (!node.children[char]) {
+            return false;
+          }
+          node = node.children[char];
+        }
+        return true;
+      }
+
     populateSuffixTree(word) {
         for (let i = 0; i < word.length; i++) {
             this.insert(word.substring(i));
         }
     }
 
+    countwords() {
+        return this.countwordsHelper(this.root);
+      }
+
+      countwordsHelper(node) {
+        node = this.root
+        if (!node) return 0;
+        let count = node.endOFWord ? 1 : 0;
+        for (let child in node.children) {
+          count += this.countwordsHelper(node.children[child]);
+        }
+        return count;
+      }
+
+      autocomplete(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+          if (node.children[char]) {
+            node = node.children[char];
+          } else {
+            return [];
+          }
+        }
+        return this.autocompleteHelper(node, prefix);
+      }
+      autocompleteHelper(node, prefix) {
+        if (!node) {
+          return;
+        }
+        let result = [];
+        if (node.isEnd) {
+          result.push(prefix);
+        }
+        for (let char in node.children) {
+          result = result.concat(
+            this.autocompleteHelper(node.children[char], prefix + char)
+          );
+        }
+        return result;
+      }
+    
+
 }
 
 // Example Usage:
 const trie = new Trie();
-trie.populateSuffixTree('sanu')
-// trie.insert("apple");
+// trie.populateSuffixTree('sanu')
+trie.insert("apple");
 // trie.insert("app");
 // trie.insert("banana");
 console.log(trie);
