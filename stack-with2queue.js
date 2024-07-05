12fb5e65a18e7baff824dfb95706b37b884d9681
AdhilKappil <adhilalikappil@gmail.com>
2024-07-05 20:57:17 +0530

    stack using 2 queue updated

--- stack-with2queue.js
+++ stack-with2queue.js
@@ -11,14 +11,12 @@ class StackUsingQueues {
             console.log("Stack is empty");
             return;
         }
-        while (this.q1.length > 1) {
+        while (this.q1.length >1) {
             this.q2.push(this.q1.shift());
         }
         const poppedElement = this.q1.shift();
         // Swap the queues
-        const temp = this.q1;
-        this.q1 = this.q2;
-        this.q2 = temp;
+        [this.q1, this.q2] = [this.q2, this.q1]
         return poppedElement;
     }
     top() {
@@ -26,16 +24,7 @@ class StackUsingQueues {
             console.log("Stack is empty");
             return;
         }
-        while (this.q1.length > 1) {
-            this.q2.push(this.q1.shift());
-        }
-        const topElement = this.q1.shift();
-        this.q2.push(topElement);
-        // Swap the queues
-        const temp = this.q1;
-        this.q1 = this.q2;
-        this.q2 = temp;
-        return topElement;
+       return this.q1[this.q1.length-1]
     }
     empty() {
         return this.q1.length === 0;
@@ -49,7 +38,3 @@ stack.push(3);
 
 console.log(stack.pop()); // 3
 console.log(stack.top()); // 2
-console.log(stack.pop()); // 2
-console.log(stack.empty()); // false
-console.log(stack.pop()); // 1
-console.log(stack.empty()); // true
